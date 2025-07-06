386f08970f9f91c046717767e02727e6a0fc373b
Terence Jiang <terencejiang2004@gmail.com>
2025-07-06 16:19:22 -0400

    starting new connection to postgres

--- plateful/src/utils/postgres.js
+++ plateful/src/utils/postgres.js
@@ -1,11 +1,11 @@
 import {Pool} from 'pg';
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'plateful',
-    database: 'PlatefulDB'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 export const query = async (text, params) => {
